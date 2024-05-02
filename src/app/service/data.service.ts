d17b4978a1dd218b66386964913e8fcbbd427a2f
Joh24 <34835271+Joh24@users.noreply.github.com>
2024-05-02 23:08:27 +0300

    Update data.service.ts
    OK

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -98,6 +98,175 @@ export class DataService {
                                 nom : "Jeanne Marie",
                                 photo : "assets/1.PNG"
                             }
+                        },{
+                            id : 8,
+                            title : "Mission 1",
+                            start: "2024-05-02 05:30:00",
+                            end: "2024-05-02 07:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#9a0",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 9,
+                            title : "Mission 2",
+                            start: "2024-05-02 11:30:00",
+                            end: "2024-05-02 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 10,
+                            title : "Mission 3",
+                            start: "2024-05-03 10:30:00",
+                            end: "2024-05-03 11:30:00",
+                            description : "Description mission 2",
+                            backgroundColor: "#380",
+                            imageurl : "assets/2.PNG",
+                            personne : {
+                                id : 2,
+                                nom : "Jean Luc",
+                                photo : "assets/2.PNG"
+                            }
+                        },{
+                            id : 11,
+                            title : "Mission 3",
+                            start: "2024-05-08 08:20:00",
+                            end: "2024-05-08 10:20:00",
+                            description : "Description mission 2",
+                            backgroundColor: "#380",
+                            imageurl : "assets/2.PNG",
+                            personne :{
+                                id : 4,
+                                nom : "Jean Petit",
+                                photo : "assets/4.PNG"
+                            }
+                        },{
+                            id :12,
+                            title : "Mission 1",
+                            start: "2024-05-08 05:30:00",
+                            end: "2024-05-08 07:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#9a0",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 13,
+                            title : "Mission 2",
+                            start: "2024-05-09 11:30:00",
+                            end: "2024-05-09 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 14,
+                            title : "Mission 2",
+                            start: "2024-05-15 11:30:00",
+                            end: "2024-05-15 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 15,
+                            title : "Mission 2",
+                            start: "2024-05-16 11:30:00",
+                            end: "2024-05-16 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 16,
+                            title : "Mission 2",
+                            start: "2024-05-17 11:30:00",
+                            end: "2024-05-17 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 17,
+                            title : "Mission 2",
+                            start: "2024-05-22 11:30:00",
+                            end: "2024-05-22 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 18,
+                            title : "Mission 2",
+                            start: "2024-05-31 11:30:00",
+                            end: "2024-05-31 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 19,
+                            title : "Mission 2",
+                            start: "2024-05-23 11:30:00",
+                            end: "2024-05-23 13:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
+                        },{
+                            id : 20,
+                            title : "Mission 2",
+                            start: "2024-05-29 16:30:00",
+                            end: "2024-05-29 17:30:00",
+                            description : "Description mission 1",
+                            backgroundColor: "#450",
+                            imageurl : "assets/1.PNG",
+                            personne : {
+                                id : 1,
+                                nom : "Jeanne Marie",
+                                photo : "assets/1.PNG"
+                            }
                         }]
 
   personne : any  = [{
